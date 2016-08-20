174a04486a2f8099e14b333097cb2146c51a69ef
Joseph Herkness <jpherkness@gmail.com>
2016-08-20 12:34:42 -0400

    Fixed the main.js script issues

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
-  $('.tooltip').tooltipster({
-    theme: 'tooltip-theme',
-    position: 'right',
-    delay: 0
-  });
 
-  // Begin Highlighting pre code blocks
-  hljs.initHighlightingOnLoad();
-
+  // Initializes the project grid
   $('.project-wrapper').masonry({
-    // set itemSelector so .grid-sizer is not used in layout
     itemSelector: '.project',
-    // use element for option
     columnWidth: '.project',
     percentPosition: true,
     gutter: 20
   })
+
 });
