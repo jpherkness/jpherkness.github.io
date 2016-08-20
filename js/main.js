ea5eb7ae2a4d979763cf3aeca8be554dcf166bcc
Joseph Herkness <jpherkness@gmail.com>
2016-08-19 22:43:11 -0400

    Portfolio design update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ $(document).ready(function() {
     position: 'right',
     delay: 0
   });
-  
+
   // Begin Highlighting pre code blocks
   hljs.initHighlightingOnLoad();
+
+  $('.project-wrapper').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.project',
+    // use element for option
+    columnWidth: '.project',
+    percentPosition: true,
+    gutter: 20
+  })
 });
