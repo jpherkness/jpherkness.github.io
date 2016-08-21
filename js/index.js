52d98d31957e16af5a49f086f717dfec3b3cb406
Joseph Herkness <jpherkness@gmail.com>
2016-08-21 18:00:46 -0400

    Added animations and compressed images

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 
 jQuery(window).on('load', function(){
+
   $('.projects-grid').masonry({
     itemSelector: '.project',
     columnWidth: '.project',
     percentPosition: true,
     gutter: 20,
-    isAnimated: true
-  })
+    isAnimated: false
+  });
 
   // Fade in the projects
-  $('.project').css('display', 'none');
-  $('.project').css('visibility', 'visible');
-  $('.project').fadeIn(1250);
+  $('.project').removeClass('tada');
+  $('.project').each(function(i){
+    setTimeout(function () {
+      $('.project').eq(i).addClass('tada');
+    }, 200 * i);
+  });
 });
